9b9b9913c9e30f706c4acbfd8176705433bf4629
wildanfauzanr <wildanfauzan2027@gmail.com>
2024-05-08 13:54:45 +0700

    SI4502KEL6-14 Menampilkan Halaman Loker Update

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,6 @@ Route::middleware(['auth', 'role:applicant'])->group(function () {
     Route::get('/applicant/dashboard', function () {
         return view('applicant.applicant_dashboard');
     });
-    Route::get('applicant/jobs', [JobController::class, 'availableJobs'])->name('applicant.jobs.index');
-    Route::post('applicant/jobs/{job}/apply', [ApplicationController::class, 'store'])->name('applicant.jobs.apply');
-    Route::get('/dashboard/applicant', [ApplicantController::class, 'index'])->name('applicant.dashboard');
-    Route::get('applicant/applications/active', [ApplicationController::class, 'activeApplications'])->name('applicant.applications.active');
-    Route::get('applicant/applications/{application}/schedule-interview', [ApplicationController::class, 'scheduleInterview'])->name('applicant.schedule.interview');
-    Route::post('applications/{application}/interviews/schedule', [InterviewController::class, 'schedule'])->name('interview.schedule');
 });
 
 // Admin
@@ -43,3 +37,10 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 Route::get('/login', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'App\Http\Controllers\Auth\LoginController@login');
 Route::post('/logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+
+Route::get('applicant/jobs', [JobController::class, 'availableJobs'])->name('applicant.jobs.index');
+Route::post('applicant/jobs/{job}/apply', [ApplicationController::class, 'store'])->name('applicant.jobs.apply');
+Route::get('/dashboard/applicant', [ApplicantController::class, 'index'])->name('applicant.dashboard');
+Route::get('applicant/applications/active', [ApplicationController::class, 'activeApplications'])->name('applicant.applications.active');
+Route::get('applicant/applications/{application}/schedule-interview', [ApplicationController::class, 'scheduleInterview'])->name('applicant.schedule.interview');
+Route::post('applications/{application}/interviews/schedule', [InterviewController::class, 'schedule'])->name('interview.schedule');
